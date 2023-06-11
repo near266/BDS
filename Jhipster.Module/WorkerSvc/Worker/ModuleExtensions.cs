35dd10fb97564f003c66ea3fbccc51df01a65ee6
Manh Dung <vinhdunghuy@gmail.com>
2023-06-11 12:18:08 +0700

    background updatestatus

---
+++ Jhipster.Module/WorkerSvc/Worker/ModuleExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WorrkerSvc.Domain.Extensions;
+using WorkerSvc.Infrastructure.Extensions;
+namespace Worker
+{
+    public static class ModuleExtensions
+    {
+        public static IServiceCollection AddWorkerSale(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddBaseCore()
+                .AddBaseInfrastructure(configuration);
+           // services.AddScoped<UpdateStatusWorker>();
+
+           services.AddHostedService<UpdateStatusWorker>();
+            return services;
+        }
+    }
+}
---
+++ Jhipster.Module/WorkerSvc/WorrkerSvc.Domain/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WorrkerSvc.Domain.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBaseCore(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
+
