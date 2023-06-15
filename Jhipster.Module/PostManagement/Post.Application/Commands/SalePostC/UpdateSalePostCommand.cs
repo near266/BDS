d1e1fcad6cac35368c448881bffa1a5792690261
DESKTOP-1KQ5V04\Thinkpad <duy0256@gmail.com>
2023-06-15 21:01:15 +0700

    update

--- Jhipster.Module/PostManagement/Post.Application/Commands/SalePostC/UpdateSalePostCommand.cs
+++ Jhipster.Module/PostManagement/Post.Application/Commands/SalePostC/UpdateSalePostCommand.cs
@@ -33,6 +33,7 @@ namespace Post.Application.Commands.SalePostC
         public string? Address { get; set; }
         public string? PhoneNumber { get; set; }
         public double? NumberOfDate { get; set; }
+        public string? Reason { get; set; }
         [JsonIgnore]
         public DateTime? LastModifiedDate { get; set; }
         [JsonIgnore]
