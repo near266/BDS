a8a30ceb011d9954eca8a57cc636e5eb4cf2e208
Manh Dung <vinhdunghuy@gmail.com>
2023-06-23 10:40:00 +0700

    Update Sale

--- Jhipster.Module/PostManagement/Post.Application/Commands/SalePostC/UpdateSalePostAdminV2C.cs
+++ Jhipster.Module/PostManagement/Post.Application/Commands/SalePostC/UpdateSalePostAdminV2C.cs
@@ -14,17 +14,17 @@ namespace Post.Application.Commands.SalePostC
     public class UpdateSalePostAdminV2C : IRequest<int>
     {
         public string Id { get; set; }
-        public int Type { get; set; }
+        public int? Type { get; set; }
         public string? Titile { get; set; }
         public string? Description { get; set; }
-        public int Status { get; set; }
-        public double Price { get; set; }
+        public int? Status { get; set; }
+        public double? Price { get; set; }
         public double? Area { get; set; }
         public string? Region { get; set; }
         public string? Ward { get; set; }
         public List<string>? Image { get; set; }
-        public int IsOwner { get; set; }
-        public string Username { get; set; }
+        public int? IsOwner { get; set; }
+        public string? Username { get; set; }
         public string? FullName { get; set; }
         public string? Email { get; set; }
         public string? Address { get; set; }
