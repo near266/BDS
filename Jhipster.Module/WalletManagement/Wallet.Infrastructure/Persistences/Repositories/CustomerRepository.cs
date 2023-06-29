72440cb6569619e67112cc1e346a9704147d7eca
Manh Dung <vinhdunghuy@gmail.com>
2023-06-29 22:41:52 +0700

    delete cus

--- Jhipster.Module/WalletManagement/Wallet.Infrastructure/Persistences/Repositories/CustomerRepository.cs
+++ Jhipster.Module/WalletManagement/Wallet.Infrastructure/Persistences/Repositories/CustomerRepository.cs
@@ -98,7 +98,7 @@ namespace Wallet.Infrastructure.Persistences.Repositories
                     query = query.Where(i => i.IsUnique == false);
                 }
             }
-            var cusquery1 = query.Where(i => i.Status == true).OrderByDescending(i => i.CreatedDate);
+            var cusquery1 = query.OrderByDescending(i => i.CreatedDate);
             var cusquery2 = await cusquery1.Skip(pagesize * (page - 1))
                                 .Take(pagesize)
                                 .ToListAsync();
