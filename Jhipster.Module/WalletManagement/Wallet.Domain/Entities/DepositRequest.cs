1ae40829d9159ade8165088436ef6b8686ef4365
Manh Dung <vinhdunghuy@gmail.com>
2023-06-29 23:04:19 +0700

    update code_deposit

--- Jhipster.Module/WalletManagement/Wallet.Domain/Entities/DepositRequest.cs
+++ Jhipster.Module/WalletManagement/Wallet.Domain/Entities/DepositRequest.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Jhipster.Domain.User;
 
 namespace Wallet.Domain.Entities
 {
     public class DepositRequest : BaseEntity<Guid>
     {
+        public string? DepositCode { get; set; } = RandomUtil.GenerateKey();
         public Guid CustomerId { get; set; }
         public string? PhoneNumber { get; set; }
         public decimal? Amount { get; set; }
