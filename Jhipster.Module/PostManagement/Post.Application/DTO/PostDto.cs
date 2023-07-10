2420437435d7bd59a93eeb65cc95216c725f400e
Manh Dung <vinhdunghuy@gmail.com>
2023-07-10 09:50:04 +0700

    add phoneNumber

--- Jhipster.Module/PostManagement/Post.Application/DTO/PostDto.cs
+++ Jhipster.Module/PostManagement/Post.Application/DTO/PostDto.cs
@@ -69,7 +69,7 @@ namespace Post.Application.DTO
         public string? FullName { get; set; }
         //public string? Email { get; set; }
         //public string? Address { get; set; }
-        //public string? PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
         //public DateTime? DueDate { get; set; }
         public string UserId { get; set; }
         public string? Reason { get; set; }
@@ -103,5 +103,5 @@ namespace Post.Application.DTO
     {
         public string? Name { get; set; }
     }
-   
+
 }
